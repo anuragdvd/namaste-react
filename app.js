6eedd8b7ac3ece8fa254ba090b1a513cca770367
anuragdvd <anurag.dwivedi@juspay.in>
2023-10-20 15:32:22 +0530

    completed basic react

--- app.js
+++ app.js
@@ -1,110 +1,107 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { restaurantList } from "./data";
 
-// React.createElement => ReactElement-JS object => HTLMELement(render)
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading",
-    xyz: "abc",
-  },
-  "Hello World"
-);
+/*
 
-console.log("This is createRoot heading " + JSON.stringify(heading));
-// root.render(heading);
-
-/* 
-
-<div id="parent">
-    <div id="child">
-        <h1> Hello world </h1>
-    </div>
-</div>
-
-*/
-/* 
-
-<div id="parent">
-    <div id="child">
-        <h1> Hello world </h1>
-        <h2> Sibling </h2>
-    </div>
-</div>
+    Header component
+        - Logo component
+        - NavItem Component
+    Body Component
+        - Search bar component
+        - ResaturantContainer Component
+            - RestroCard Component
+                - Image
+                - Name
+                - Del. Time
+    Footer 
+        - Copy Right
+        - Links
+        - Address
+        - Contact
 
 */
 
-// const root2 = ReactDOM.createRoot(document.getElementById("root2"))
-
-// root2.render(React.createElement("div", { id :"parent" },
-//     React.createElement("div",{id:"child"},
-//        [ React.createElement("h1",{},"Testing new thing"),
-//             React.createElement("h2", {}, "This is working")])));
-
-console.log("hello");
-
-const jsxHeading = <h1 className="heading">Welcome to JSX!</h1>;
-
-// If in mutliple lines then you need to add ()
-// React Element
-const jsxHeading2 = (
-  <h1 className="heading" tabIndex="5">
-    Welcome to JSX!
-  </h1>
-);
-
-// JSX (transpiled before it reaches JS) - transalation is done by Babel
-// JSX => React.createElement => ReactElement-JS object => HTMLElement(render)
-console.log(jsxHeading);
-
-// root.render(React.createElement("div", { id :"parent" },
-//     React.createElement("div",{id:"child"},
-//         React.createElement("h1",{},"Testing new thing"))));
-
-// React Functional Component
-// A function that returns JSX
-
-const HeadingComponent2 = () => {
-  return <h1 className="header">Better Arrow Functions</h1>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://dynamic.brandcrowd.com/asset/logo/a9ee4b5e-a3a6-4c0a-9e24-e9b389587790/logo-search-grid-1x?logoTemplateVersion=1&v=637781767973570000"
+        ></img>
+      </div>
+      <div className="navBar">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const Title = function () {
-  return <h1>Blowing react</h1>;
+// Writing CSS inside JSX by creating them JS objects
+const styleCard = {
+  backgroundColor: "#f0f0f0",
 };
 
-// React component
-const title = <h1> This is a React Component compisition </h1>;
+const RestroCard = (props) => {
+  const { resData } = props;
+  const {
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+    cloudinaryImageId,
+  } = resData?.data;
+  return (
+    <div className="restro-card" style={styleCard}>
+      <img
+        className="cardimg"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      ></img>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{deliveryTime} minutes</h4>
+    </div>
+  );
+};
 
-// React Component compostion
-const title2 = <h1>This is React Component wow {title}</h1>;
+const Body = () => {
+  return (
+    <div className="content">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {restaurantList.map((restaurant, index) => (
+          // Never use indexes as keys
+          // https://robinpokorny.com/blog/index-as-a-key-is-an-anti-pattern/
+          <RestroCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-const number = 10000;
-// Very simple to compose components -> <Title /> or <Title></Title> or even {Title()}
-// You can also write JS inside these functions by using {}
-// You can also compose it with React component
-const HeadingComponent = () => (
-  <div id="container">
-    <Title></Title>
-    <Title />
-
-    <h2> React JSX is pure magic {number + 233} </h2>
-    <h3>{console.log("Will console log work")}</h3>
-    <h1 className="heading">Welcome to React</h1>
-    {title2}
-    {Title()}
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {Header()}
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(jsxHeading);
 
-// Syntax to render functional component
-console.log("about to render");
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
 
-// const data = api.getData();
-
-// What if the api is malicious ? As we are executing the JS code that is present in JSX
-// JSX will take care of these data
-// IT will sanitize the data and then pass it
-// So JSX will provide XSS
+// not using keys (not-acceptable) <<< index as key <<< unique key
---
+++ theory.js
@@ -0,0 +1,135 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React.createElement => ReactElement-JS object => HTLMELement(render)
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+    xyz: "abc",
+  },
+  "Hello World"
+);
+
+console.log("This is createRoot heading " + JSON.stringify(heading));
+// root.render(heading);
+
+/* 
+
+<div id="parent">
+    <div id="child">
+        <h1> Hello world </h1>
+    </div>
+</div>
+
+*/
+/* 
+
+<div id="parent">
+    <div id="child">
+        <h1> Hello world </h1>
+        <h2> Sibling </h2>
+    </div>
+</div>
+
+*/
+
+// const root2 = ReactDOM.createRoot(document.getElementById("root2"))
+
+// root2.render(React.createElement("div", { id :"parent" },
+//     React.createElement("div",{id:"child"},
+//        [ React.createElement("h1",{},"Testing new thing"),
+//             React.createElement("h2", {}, "This is working")])));
+
+console.log("hello");
+
+const jsxHeading = <h1 className="heading">Welcome to JSX!</h1>;
+
+// If in mutliple lines then you need to add ()
+// React Element
+const jsxHeading2 = (
+  <h1 className="heading" tabIndex="5">
+    Welcome to JSX!
+  </h1>
+);
+
+// JSX (transpiled before it reaches JS) - transalation is done by Babel
+// JSX => React.createElement => ReactElement-JS object => HTMLElement(render)
+console.log(jsxHeading);
+
+// root.render(React.createElement("div", { id :"parent" },
+//     React.createElement("div",{id:"child"},
+//         React.createElement("h1",{},"Testing new thing"))));
+
+// React Functional Component
+// A function that returns JSX
+
+const HeadingComponent2 = () => {
+  return <h1 className="header">Better Arrow Functions</h1>;
+};
+
+const Title = function () {
+  return <h1>Blowing react</h1>;
+};
+
+// React component
+const title = <h1> This is a React Component compisition </h1>;
+
+// React Component compostion
+const title2 = <h1>This is React Component wow {title}</h1>;
+
+const number = 10000;
+// Very simple to compose components -> <Title /> or <Title></Title> or even {Title()}
+// You can also write JS inside these functions by using {}
+// You can also compose it with React component
+
+const hack =
+  "function abc () { <img onerror='alert(\"Hacked!\");' src='invalid-image' />}; abc();";
+
+// abc = (x) => {
+//   debugger;
+//   return x * 100;
+// };
+
+// window.payload = 1023;
+
+const abc = (function () {
+  console.log("what is wrong");
+})();
+
+const HeadingComponent = () => (
+  <div id="container">
+    {/* {hack} */}
+    {/* <div
+      dangerouslySetInnerHTML={{
+        __html: hack,
+      }}
+    ></div> */}
+    {/* {(function () {
+      console.log("what is wrong");
+    })()} */}
+    {abc}
+    <Title></Title>
+    <Title />
+
+    <h2> React JSX is pure magic {number - 23 + 233} </h2>
+    <h3>{console.log("Will console log work")}</h3>
+    <h1 className="heading">Welcome to React</h1>
+    {title2}
+    {Title()}
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+
+// Syntax to render functional component
+console.log("about to render");
+root.render(<HeadingComponent />);
+
+// const data = api.getData();
+
+// What if the api is malicious ? As we are executing the JS code that is present in JSX
+// JSX will take care of these data
+// IT will sanitize the data and then pass it
+// So JSX will provide XSS
